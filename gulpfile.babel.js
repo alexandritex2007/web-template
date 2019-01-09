be9bec09366045870b76607ba87832031befd637
Masakazu Kurokawa <kurokawa@senku.jp>
2019-01-09 14:00:00 +0900

    gulp 4系対応

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -25,66 +25,72 @@ const SRC = 'src',
 const ectConfig = require('./src/include/_const.js');
 
 // ect
-gulp.task('ect', () => {
+gulp.task('ect', (done) => {
     gulp.src('src/ect/**/*.ect')
-    .pipe(plumber())
-    .pipe(ect({data(file, cb) {
-        cb(ectConfig);
-    }}))
-    .pipe(gulp.dest(DIST))
-    .pipe(browser.reload({stream:true}));
+        .pipe(plumber())
+        .pipe(ect({data(file, cb) {
+            cb(ectConfig);
+        }}))
+        .pipe(gulp.dest(DIST))
+        .pipe(browser.reload({stream:true}));
+    done();
+    
 });
 
 // sass
-gulp.task('sass', () => {
+gulp.task('sass', (done) => {
     gulp.src(SRC + '/sass/**/*.scss')
-    .pipe(plumber())
-    .pipe(frontnote({ css: 'site/css/common.css' }))
-    .pipe(sourcemaps.init())
-    .pipe(bulkSass())
-    .pipe(sass({outputStyle: 'expanded'})) //nested,compact,expanded,compressed
-    .pipe(autoprefixer({
-        // ☆IEは9以上、Androidは4以上、iOS Safariは8以上
-        // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
-        browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4','ios_saf >= 8'],
-        cascade: false
-    }))
-    .pipe(sourcemaps.write('../maps/'))
-    .pipe(gulp.dest('guide/site/css'))
-    .pipe(gulp.dest(DIST + '/site/css'))
-    .pipe(browser.reload({stream:true}));
+        .pipe(plumber())
+        .pipe(frontnote({ css: 'site/css/common.css' }))
+        .pipe(sourcemaps.init())
+        .pipe(bulkSass())
+        .pipe(sass({outputStyle: 'expanded'})) //nested,compact,expanded,compressed
+        .pipe(autoprefixer({
+            // ☆IEは9以上、Androidは4以上、iOS Safariは8以上
+            // その他は最新2バージョンで必要なベンダープレフィックスを付与する設定
+            browsers: ['last 2 versions', 'ie >= 9', 'Android >= 4','ios_saf >= 8'],
+            cascade: false
+        }))
+        .pipe(sourcemaps.write('../maps/'))
+        .pipe(gulp.dest('guide/site/css'))
+        .pipe(gulp.dest(DIST + '/site/css'))
+        .pipe(browser.reload({stream:true}));
+    done();
 });
 
 //browser sync
-gulp.task('server', () => {
+gulp.task('server', (done) => {
     browser({
         server: {
             baseDir: DIST
         }
     });
+    done();
 });
 
 //CSSmin
-gulp.task('cssmin', () => {
+gulp.task('cssmin', (done) => {
     gulp.src(SRC + '/css/**/*.css')
-    .pipe(cssmin())
-    .pipe(rename({suffix: '.min'}))
-    .pipe(gulp.dest('guide/site/css/min'))
-    .pipe(gulp.dest(DIST + '/assets/css/min'));
+        .pipe(cssmin())
+        .pipe(rename({suffix: '.min'}))
+        .pipe(gulp.dest('guide/site/css/min'))
+        .pipe(gulp.dest(DIST + '/assets/css/min'));
+    done();
 });
 
 //webpack
 const webpackConfig = require('./webpack.config');
 
-gulp.task('webpack', () => {
+gulp.task('webpack', (done) => {
     gulp.src('./src/js/entry.js')
-    .pipe(plumber())
-    .pipe(webpackStream(webpackConfig, webpack))
-    .pipe(gulp.dest(DIST + '/site/js/'));
+        .pipe(plumber())
+        .pipe(webpackStream(webpackConfig, webpack))
+        .pipe(gulp.dest(DIST + '/site/js/'));
+    done();
 });
 
 //sprite-pc
-gulp.task('sprite', () => {
+gulp.task('sprite', (done) => {
     const spriteData = gulp.src(SRC + '/sprite/*.png')
     .pipe(spritesmith({
         imgName: 'sprite.png',
@@ -103,85 +109,85 @@ gulp.task('sprite', () => {
     }));
     spriteData.img.pipe(gulp.dest(SRC + '/img/assets/'));
     spriteData.css.pipe(gulp.dest(SRC + '/sass/'));
+    done();
 });
 
 //sprite_sp
-gulp.task('sprite_sp', () => {
+gulp.task('sprite_sp', (done) => {
     const spriteData = gulp.src(SRC + '/sprite_sp/*.png')
-    .pipe(spritesmith({
-        imgName: 'sprite_sp.png',
-        cssName: '_sprite_sp.scss',
-        padding: 20,
-        imgPath: '../../assets/images/sprite_sp.png',
-        cssFormat: 'scss',
-        algorithm:'top-down',
-        cssOpts: {
-            functions: false
-        },
-        cssVarMap(sprite) {
-            sprite.name = 'sprite-' + sprite.name;
-        }
-    }));
+        .pipe(spritesmith({
+            imgName: 'sprite_sp.png',
+            cssName: '_sprite_sp.scss',
+            padding: 20,
+            imgPath: '../../assets/images/sprite_sp.png',
+            cssFormat: 'scss',
+            algorithm:'top-down',
+            cssOpts: {
+                functions: false
+            },
+            cssVarMap(sprite) {
+                sprite.name = 'sprite-' + sprite.name;
+            }
+        }));
     spriteData.img.pipe(gulp.dest(SRC + '/img/assets/'))
     spriteData.img.pipe(gulp.dest('guide/assets/images/'))
     spriteData.css.pipe(gulp.dest(SRC + '/sass/'));
+    done();
 });
 
 //img_min
-gulp.task('tinypng', () => {
+gulp.task('tinypng', (done) => {
     gulp.src(SRC + '/img/assets/**/*.{png,jpg,jpeg}')
-    .pipe(tinyping({
-        key: 'llZEaFTtjee2TrIOmoBP25AkPKY5BhCW'
-    }))
-    .pipe(gulp.dest('guide/assets/images/'))
-    .pipe(gulp.dest(DIST + '/assets/images/'));
+        .pipe(tinyping({key: 'llZEaFTtjee2TrIOmoBP25AkPKY5BhCW'}))
+        .pipe(gulp.dest('guide/assets/images/'))
+        .pipe(gulp.dest(DIST + '/assets/images/'));
 
     gulp.src(SRC + '/img/content/**/*.{png,jpg,jpeg}')
-    .pipe(tinyping({
-        key: 'llZEaFTtjee2TrIOmoBP25AkPKY5BhCW'
-    }))
-    .pipe(gulp.dest('guide/assets/images/'))
-    .pipe(gulp.dest(DIST + '/content/images/'));
+        .pipe(tinyping({key: 'llZEaFTtjee2TrIOmoBP25AkPKY5BhCW'}))
+        .pipe(gulp.dest('guide/assets/images/'))
+        .pipe(gulp.dest(DIST + '/content/images/'));
+    done();
 });
 
 // svg
 gulp.task('svgstore', () => {
   return gulp
   .src(SRC + '/svg/**/*.svg')
-  .pipe(svgmin((file) => {
-      const prefix = path.basename(file.relative, path.extname(file.relative));
-      return {
-          plugins: [{
-              cleanupIDs: {
-                  prefix: prefix + '-',
-                  minify: true
-              }
-          }]
-      }
-  }))
-  .pipe(svgstore({ inlineSvg: true }))
+    .pipe(svgmin((file) => {
+        const prefix = path.basename(file.relative, path.extname(file.relative));
+        return {
+            plugins: [{
+                cleanupIDs: {
+                    prefix: prefix + '-',
+                    minify: true
+                }
+            }]
+        }
+    }))
+    .pipe(svgstore({ inlineSvg: true }))
 
-  .pipe(cheerio({
-      run($) {
-          $('[fill]').removeAttr('fill');
-          $('[stroke]').removeAttr('stroke');
-          $('svg').attr('style','display:none');
-      },
-      parserOptions: { xmlMode: true }
-  }))
-  .pipe(gulp.dest(DIST + '/assets/svg/'));
+    .pipe(cheerio({
+        run($) {
+            $('[fill]').removeAttr('fill');
+            $('[stroke]').removeAttr('stroke');
+            $('svg').attr('style','display:none');
+        },
+        parserOptions: { xmlMode: true }
+    }))
+    .pipe(gulp.dest(DIST + '/assets/svg/'));
 });
 
 
-//watch
-gulp.task('default',['server'], () => {
-    gulp.watch(SRC + '/sass/**/*.scss',['sass']);
-    gulp.watch(SRC + '/css/**/*.css',['cssmin']);
-    gulp.watch(SRC + '/js/entry.js', ['webpack']);
-    gulp.watch(SRC + '/sprite/*.png',['sprite']);
-    gulp.watch(SRC + '/sprite_sp/*.png',['sprite_sp']);
-    gulp.watch(SRC + '/svg/*.svg',['svgstore']);
-    gulp.watch(SRC + '/img/**/*.{png,jpg,jpeg}',['tinypng']);
-    gulp.watch(SRC + '/ect/**/*.ect', ['ect']);
+// watch
+gulp.task('watch', () => {
+    gulp.watch(SRC + '/sass/**/*.scss',gulp.parallel('sass'));
+    gulp.watch(SRC + '/css/**/*.css',gulp.parallel('cssmin'));
+    gulp.watch(SRC + '/js/entry.js', gulp.parallel('webpack'));
+    gulp.watch(SRC + '/sprite/*.png',gulp.parallel('sprite'));
+    gulp.watch(SRC + '/sprite_sp/*.png',gulp.parallel('sprite_sp'));
+    gulp.watch(SRC + '/svg/*.svg',gulp.parallel('svgstore'));
+    gulp.watch(SRC + '/img/**/*.{png,jpg,jpeg}',gulp.parallel('tinypng'));
+    gulp.watch(SRC + '/ect/**/*.ect',gulp.parallel('ect'));
 });
+gulp.task("default",gulp.series(gulp.parallel("server", "watch")));
 
